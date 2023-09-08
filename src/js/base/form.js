d051350d780510e34260004ca28a426da101c942
Barış Hakverdi <info@barishakverdi.com>
2023-09-08 22:47:36 +0300

    update

--- src/js/base/form.js
+++ src/js/base/form.js
@@ -1,65 +1,68 @@
 const contactForm = document.getElementById("contact-form");
-contactForm.addEventListener("submit", function (e) {
-    e.preventDefault();
 
-    // [...this.elements].forEach(formElement => {
-    //     if (!formElement.checkValidity()) {
-    //         console.log(formElement.validationMessage);
-    //     } else {
-    //
-    //     }
-    // })
+if (contactForm) {
+    contactForm.addEventListener("submit", function (e) {
+        e.preventDefault();
 
-    [...this.elements].forEach(formElement => formControl(formElement));
+        // [...this.elements].forEach(formElement => {
+        //     if (!formElement.checkValidity()) {
+        //         console.log(formElement.validationMessage);
+        //     } else {
+        //
+        //     }
+        // })
 
-    if (this.checkValidity() === true) {
-        console.log("Başarılı!");
-        new FormData(contactForm);
-    }
+        [...this.elements].forEach(formElement => formControl(formElement));
 
-}, true);
-
-contactForm.addEventListener('formdata', function(e) {
-
-    // tüm form datamızı temsil ediyor
-    let data = e.formData;
-
-    // dilerseniz bunu bir ajax isteğiyle taçlandırabilirsiniz
-    var request = new XMLHttpRequest();
-    request.open("POST", "/ajax.php");
-    request.addEventListener('load', function(response) {
-        console.log(response)
-    });
-    request.send(data);
-});
-
-[...contactForm.elements].forEach(formElement => {
-    ['change', 'keyup'].forEach(method => formElement.addEventListener(method, e => formControl(e.target)));
-})
-
-function formControl(formElement) {
-    if (!formElement.checkValidity()) {
-        let error;
-
-        if (formElement.nextElementSibling?.className === "error-msg") {
-            error = formElement.nextElementSibling;
-        } else {
-            error = document.createElement("small");
+        if (this.checkValidity() === true) {
+            console.log("Başarılı!");
+            new FormData(contactForm);
         }
 
-        console.log(error);
+    }, true);
 
-        error.className = "error-msg";
-        error.innerText = formElement.validationMessage;
-        formElement.insertAdjacentElement("afterend", error);
-        formElement.classList.add("error");
+    contactForm.addEventListener('formdata', function(e) {
 
-    } else {
-        const errorMsg = formElement.nextElementSibling;
+        // tüm form datamızı temsil ediyor
+        let data = e.formData;
 
-        if (errorMsg) {
-            errorMsg.remove();
-            formElement.classList.remove("error");
+        // dilerseniz bunu bir ajax isteğiyle taçlandırabilirsiniz
+        var request = new XMLHttpRequest();
+        request.open("POST", "/ajax.php");
+        request.addEventListener('load', function(response) {
+            console.log(response)
+        });
+        request.send(data);
+    });
+
+    [...contactForm.elements].forEach(formElement => {
+        ['change', 'keyup'].forEach(method => formElement.addEventListener(method, e => formControl(e.target)));
+    })
+
+    function formControl(formElement) {
+        if (!formElement.checkValidity()) {
+            let error;
+
+            if (formElement.nextElementSibling?.className === "error-msg") {
+                error = formElement.nextElementSibling;
+            } else {
+                error = document.createElement("small");
+            }
+
+            console.log(error);
+
+            error.className = "error-msg";
+            error.innerText = formElement.validationMessage;
+            formElement.insertAdjacentElement("afterend", error);
+            formElement.classList.add("error");
+
+        } else {
+            const errorMsg = formElement.nextElementSibling;
+
+            if (errorMsg) {
+                errorMsg.remove();
+                formElement.classList.remove("error");
+            }
         }
     }
 }
