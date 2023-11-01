f77f0eb7dc0cad879316f0ab6078a7b48a3186dd
barishakverdi <info@barishakverdi.com>
2023-11-01 16:56:31 +0300

    build fix fix

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import handlebars from 'vite-plugin-handlebars';
 export default defineConfig({
     build: {
         minify: true,
-        outDir: "dist/",
+        outDir: "../dist/",
         cssCodeSplit: false,
         manifest: true,
 
