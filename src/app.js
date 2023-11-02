6de273e5c6c94ccd330b7ad7ddf71b47afa6460c
barishakverdi <info@barishakverdi.com>
2023-11-02 13:13:50 +0300

    some seo update

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 if (window.location.pathname === "/") window.location.assign("views/");
+if (window.location.href.indexOf("localhost") > -1 || window.location.href.indexOf("vite-starter") > -1) document.querySelector('[rel="canonical"]').setAttribute("href", window.location.href);
 import('./js/base/header.js');
 import('./js/base/carousel.js');
 import('./js/base/fancybox.js');
--- src/js/base/modal.js
+++ src/js/base/modal.js
@@ -1,46 +1,48 @@
 import {body} from "./header.js";
 let modalContainer = document.querySelector(".modal-container");
-let modal = modalContainer.querySelectorAll(".modal");
-let modalTriggerButton = document.querySelectorAll(".modal-trigger-button");
-let modalCloseButton = modalContainer.querySelectorAll(".close-button");
+if (modalContainer) {
+    let modal = modalContainer.querySelectorAll(".modal");
+    let modalTriggerButton = document.querySelectorAll(".modal-trigger-button");
+    let modalCloseButton = modalContainer.querySelectorAll(".close-button");
 
-function closeContainer() {
-    modalContainer.classList.add("closed");
-    body.classList.remove("overflow-hidden");
+    function closeContainer() {
+        modalContainer.classList.add("closed");
+        body.classList.remove("overflow-hidden");
+        modal.forEach(modal => {
+            modal.classList.add("closed");
+            modal.classList.remove("opened");
+        })
+    }
+
+    function openContainer(trigger) {
+        modalContainer.classList.toggle("closed");
+        body.classList.toggle("overflow-hidden");
+
+        modal.forEach(modal => {
+            if (trigger.getAttribute("modal-trigger-id") === modal.getAttribute("modal-id")) {
+                modal.classList.remove("closed");
+                modal.classList.add("opened");
+            }
+        })
+    }
+
+    modalTriggerButton.forEach(button => {
+        button.addEventListener("click", function () {
+            openContainer(button)
+        })
+    })
+
     modal.forEach(modal => {
-        modal.classList.add("closed");
-        modal.classList.remove("opened");
+        body.addEventListener("keydown", function (e) {
+            if (modalContainer.getAttribute("data-keypress-close") === "true" && e.key === "Escape") {
+                closeContainer()
+            }
+        })
     })
-}
 
-function openContainer(trigger) {
-    modalContainer.classList.toggle("closed");
-    body.classList.toggle("overflow-hidden");
-
-    modal.forEach(modal => {
-        if (trigger.getAttribute("modal-trigger-id") === modal.getAttribute("modal-id")) {
-            modal.classList.remove("closed");
-            modal.classList.add("opened");
-        }
-    })
-}
-
-modalTriggerButton.forEach(button => {
-    button.addEventListener("click", function () {
-        openContainer(button)
-    })
-})
-
-modal.forEach(modal => {
-    body.addEventListener("keydown", function (e) {
-        if (modalContainer.getAttribute("data-keypress-close") === "true" && e.key === "Escape") {
+    modalCloseButton.forEach(closeButton => {
+        closeButton.addEventListener("click", function() {
             closeContainer()
-        }
+        })
     })
-})
-
-modalCloseButton.forEach(closeButton => {
-    closeButton.addEventListener("click", function() {
-        closeContainer()
-    })
-})
+}
